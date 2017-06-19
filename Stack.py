bd6cb54218920389fe9d61fe0f6ee684e9712fc6
Parag Mali <parag@Parags-MacBook-Pro.local>
2017-06-19 22:19:50 +0530

    Data Structures and Algorithms in Python

---
+++ Stack.py
@@ -0,0 +1,37 @@
+class Stack:
+	def __init__(self):
+		self.stack = []
+	
+	def push(self, val):
+		self.stack.append(val)
+	
+	def pop(self):
+		return self.stack.pop(len(self.stack)-1)
+
+	def __str__(self):
+		return str(self.stack)
+
+	def size(self):
+		return len(self.stack)
+
+	def peek(self):
+		return self.stack[len(self.stack)-1]
+
+s = Stack()
+s.push(5)
+print(s)
+s.push(10)
+print(s)
+s.push(15)
+print(s)
+
+print(s)
+print(s.pop())
+print(s)
+
+print(s.peek())
+
+print(s)
+print(s.pop())
+print(s.pop())
+print(s.peek())
